1d9793859602da5cc4e729c2a197cadb226465e6
rRuy Castilho Barrichelo <ruy.castilho26@gmail.com>
2017-11-22 09:46:33 -0200

    Added singularity install script

---
+++ scripts/install_singularity.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Pass VERSION as parameter
+
+VERSION=2.4
+
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
