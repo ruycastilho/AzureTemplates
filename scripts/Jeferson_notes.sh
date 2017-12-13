9f116d60a8e19fa9b428c289e76c65657953bc20
Your Name <you@example.com>
2017-12-13 12:13:05 -0200

    Add mpirun example

--- scripts/Jeferson_notes.sh
+++ scripts/Jeferson_notes.sh
@@ -6,3 +6,6 @@ sudo mount -t cifs //test1diag281.file.core.windows.net/shared-fs mountpoint/ -o
 singularity image.create -s 2048 mountpoint/Centos-7.img
 # Pull the image from Docker preository
 sudo singularity build --writable mountpoint/Centos-7.img docker://centos:7
+
+
+mpirun -np 20 -hosts 10.0.0.4,10.0.0.5 singularity exec ~/mountpoint/ubuntu.img ~/mountpoint/matix_mul_mpi/matrix_mul
